f87d5ddf165e0666ff402351f108ebed8698c4cc
emilio barrera <barrera_emilio@hotmail.com>
2019-02-18 17:25:56 +0000

    Api ProductsController

---
+++ ShopWeb/Controllers/API/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿namespace ShopWeb.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ShopWeb.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        #region Attributtes
+        private readonly IProductRepository productRepository; 
+        #endregion
+        #region Constructor
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        #endregion
+
+
+        #region Methods
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            
+            return Ok(this.productRepository.GetAll()); 
+        }
+
+        #endregion
+    }
+}
