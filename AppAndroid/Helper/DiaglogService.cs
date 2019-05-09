08130f4905da167fc320db51143b5e02cecb6c8f
emilio barrera <barrera_emilio@hotmail.com>
2019-05-09 20:08:31 +0100

    Login Android Layout

---
+++ AppAndroid/Helper/DiaglogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AppAndroid.Helper
+{
+    public class DiaglogService
+    {
+        public static void ShowMessage(Context context, string title, string message, string button)
+        {
+            new AlertDialog.Builder(context)
+                .SetPositiveButton(button, (sent, args) => { })
+                .SetMessage(message)
+                .SetTitle(title)
+                .SetCancelable(false)
+                .Show();
+        }
+    }
+}
--- ShopUIClassic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ShopUIClassic
-{
-    public class Class1
-    {
-    }
-}
