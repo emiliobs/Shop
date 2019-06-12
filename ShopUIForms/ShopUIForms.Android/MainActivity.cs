c63fa3ef4bae17f0f8350dc57aa2cacc97035777
emilio barrera <barrera_emilio@hotmail.com>
2019-06-12 01:16:31 +0100

    Application  Icon

--- ShopUIForms/ShopUIForms.Android/MainActivity.cs
+++ ShopUIForms/ShopUIForms.Android/MainActivity.cs
@@ -7,7 +7,7 @@
     using Android.OS;
     using Plugin.CurrentActivity;
 
-    [Activity(Label = "ShopUIForms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Shop", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
