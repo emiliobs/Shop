202701467d2968b2af80989b5bc6e373f66f7d6f
emilio barrera <barrera_emilio@hotmail.com>
2019-07-04 11:08:15 +0100

    MVVM Cross

---
+++ FourWays.Core/App.cs
@@ -0,0 +1,23 @@
+﻿namespace FourWays.Core
+{
+    using FourWays.Core.ViewModels;
+    using MvvmCross.IoC;
+    using MvvmCross.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            this.CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            this.RegisterAppStart<TipViewModel>();
+
+            base.Initialize();
+        }
+    }
+}
